a3910a1acd47c490210b58357053f2a3612ee006
nswon <swnam@gmail.com>
2022-05-25 20:45:53 +0900

    Posts 엔티티 dev

---
+++ localhost/src/main/java/bssm/localhost/domain/Posts.java
@@ -0,0 +1,30 @@
+package bssm.localhost.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Posts {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long posts_id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+//    private User author;
+
+    private String comment;
+}
+
