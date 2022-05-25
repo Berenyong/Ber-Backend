09bb57374bb66e61fba0b43db628bcbaecf5a12c
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-05-25 19:55:41 +0900

    SET :: (#1)Requirements

---
+++ localhost/src/main/java/bssm/localhost/LocalhostApplication.java
@@ -0,0 +1,13 @@
+package bssm.localhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalhostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalhostApplication.class, args);
+	}
+
+}
---
+++ localhost/src/test/java/bssm/localhost/LocalhostApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.localhost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalhostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
